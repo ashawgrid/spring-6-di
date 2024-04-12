415bdd4a9673651f077ccfcdc0f0ae1accc59dce
ashawgrid <ashaw@griddynamics.com>
2024-04-12 16:57:14 +0530

    Dependency Injection

--- src/main/java/guru/springframework/spring6di/controllers/EnvironmentController.java
+++ src/main/java/guru/springframework/spring6di/controllers/EnvironmentController.java
@@ -3,9 +3,6 @@ package guru.springframework.spring6di.controllers;
 import guru.springframework.spring6di.services.EnvironmentService;
 import org.springframework.stereotype.Controller;
 
-/**
- * Created by jt, Spring Framework Guru.
- */
 @Controller
 public class EnvironmentController {
 
